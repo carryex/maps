0e044e81d99bfaf0d5b3fd8f63acf2c4faf53146
Daniil Pokinsokha <carryex@returns.co>
2021-11-06 12:10:50 +0100

    add google map

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import {User} from './User';
-import { Company } from './Company';
+// import {User} from './User';
+// import { Company } from './Company';
 
-const user = new User();
-console.log(user);
-
-const company = new Company();
-console.log(company);
+const div = document.getElementById('map');
+const options = {zoom: 15, center: {lat: 42.2841881627579, lng: 18.861337962762263}};
+const map = new google.maps.Map(div, options);
